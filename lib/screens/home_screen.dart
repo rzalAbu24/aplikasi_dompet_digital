c1a3b74bac799a7d1f649e3b47bd89819945a162
rzalAbu24 <rizalabu024@gmail.com>
2023-12-27 11:21:45 +0800

    Update Home Screen dan Tambahan Poster

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
+import 'package:aplikasi_dompet_digital/screens/topup.dart';
+import 'package:aplikasi_dompet_digital/screens/transfer.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   var height, width;
 
-  List gambar = ["emas.png", "listrik.png", "rupiah.png", "topup.png"];
+  List gambar = ["mcd.jpg", "kfc.jpg"];
   @override
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
@@ -28,22 +30,13 @@ class HomeScreen extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Padding(
-                          padding: EdgeInsets.only(left: 3, bottom: 0),
-                          child: Image(
-                              image: AssetImage(
-                                'images/rupiah.png',
-                              ),
-                              width: 30),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 0,
-                          ),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: width * 0.01),
                           child: Text(
-                            " 1.000.000",
+                            " Rp. 5.000.000.000",
                             style: TextStyle(
                               fontSize: 20,
-                              color: Colors.amber[400],
+                              color: Colors.white,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
@@ -64,7 +57,7 @@ class HomeScreen extends StatelessWidget {
                   height: 5,
                 ),
                 Container(
-                  height: height * 0.07,
+                  height: 55,
                   width: width * 0.9,
                   decoration: BoxDecoration(
                     color: Colors.white,
@@ -99,170 +92,197 @@ class HomeScreen extends StatelessWidget {
                   height: 5,
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 10),
+                  padding: EdgeInsets.symmetric(
+                      horizontal: width * 0.001, vertical: height * 0.01),
                   child: Container(
-                    padding: EdgeInsets.only(
-                        top: 0, bottom: 10, left: 10, right: 10),
+                    padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(30),
                         border: Border.all(color: Colors.black, width: 2),
                         color: Colors.white),
-                    child: Column(children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: 30),
-                            child: Column(
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/transfer.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: width * 0.02,
+                                    vertical: height * 0.01),
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('images/transfer.png'),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => Transfer(),
+                                            ));
+                                      },
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Transfer",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  "Transfer",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                              left: 10,
-                            ),
-                            child: Column(
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/topup.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: width * 0.02,
+                                  vertical: height * 0.01,
                                 ),
-                                Text(
-                                  "Top Up",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                              left: 10,
-                            ),
-                            child: Column(
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/listrik.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('images/topup.png'),
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => TopUp(),
+                                            ));
+                                      },
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Top Up",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  "Listrik",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                              left: 10,
-                            ),
-                            child: Column(
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/air.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: width * 0.02,
+                                  vertical: height * 0.01,
                                 ),
-                                Text(
-                                  "Air",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: 30, top: 10),
-                            child: Column(
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/pulsa.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('images/listrik.png'),
+                                      onPressed: () {},
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Listrik",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  "Pulsa",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                              top: 10,
-                              left: 10,
-                            ),
-                            child: Column(
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/telkom.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: width * 0.02,
+                                  vertical: height * 0.01,
                                 ),
-                                Text(
-                                  "Telkom",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                              top: 10,
-                              left: 10,
-                            ),
-                            child: Column(
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/pendidikan.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('images/air.png'),
+                                      onPressed: () {},
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Air",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  "Study",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                              top: 10,
-                              left: 10,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                IconButton(
-                                  icon: Image.asset('images/kredit.png'),
-                                  onPressed: () {},
-                                  iconSize: 40,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: width * 0.02,
+                                    vertical: height * 0.01),
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('images/pulsa.png'),
+                                      onPressed: () {},
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Pulsa",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  "Kredit",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: width * 0.02,
+                                    vertical: height * 0.01),
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('images/telkom.png'),
+                                      onPressed: () {},
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Telkom",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: width * 0.02,
+                                    vertical: height * 0.01),
+                                child: Column(
+                                  children: [
+                                    IconButton(
+                                      icon:
+                                          Image.asset('images/pendidikan.png'),
+                                      onPressed: () {},
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Study",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: width * 0.02,
+                                    vertical: height * 0.01),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      icon: Image.asset('images/kredit.png'),
+                                      onPressed: () {},
+                                      iconSize: 40,
+                                    ),
+                                    Text(
+                                      "Kredit",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ]),
+                        ]),
                   ),
                 ),
               ],
@@ -302,7 +322,7 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
                 SizedBox(
-                  height: 200,
+                  height: 250,
                   child: ListView.builder(
                       scrollDirection: Axis.horizontal,
                       shrinkWrap: true,
@@ -316,7 +336,7 @@ class HomeScreen extends StatelessWidget {
                           child: InkWell(
                             onTap: () {},
                             child: Container(
-                              width: width * 0.5,
+                              width: width * 0.6,
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(25),
                                   color: Colors.white,
