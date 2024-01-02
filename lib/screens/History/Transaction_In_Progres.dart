1ced8aeb5b9382b6c9cc82bf6c94a35c2bfcca1c
ahmadrohid <ahmadrohid666@gmail.com>
2024-01-02 11:47:57 +0800

    tugasewallet

---
+++ lib/screens/History/Transaction_In_Progres.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:aplikasi_dompet_digital/screens/history.dart';
+
+class TransactionInProgres extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "There is nothing here yet",
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        const Text(
+          "You have completed eveything!",
+          style: TextStyle(
+            fontSize: 12.0,
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -1,8 +1,35 @@
+import 'package:aplikasi_dompet_digital/screens/History/Transaction_Completed.dart';
+import 'package:aplikasi_dompet_digital/screens/History/Transaction_In_Progres.dart';
 import 'package:flutter/material.dart';
 
 class History extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return DefaultTabController(length: 2, 
+    child: Scaffold(
+      appBar: AppBar( backgroundColor: Color(0xFF4527A0),
+      automaticallyImplyLeading: false,
+      bottom: const TabBar(
+        tabs: [
+          Tab(
+              text: "In Progress",
+        ),
+          Tab(
+              text: "Completed",
+          ),
+      ],
+    ),
+    title: const Text(
+      'Transaction History'  
+      ),
+    ),
+    body: TabBarView(
+      children: [
+      TransactionInProgres(),
+      TransactionCompleted(),
+      ]),
+      ),
+    );
+    
   }
 }
